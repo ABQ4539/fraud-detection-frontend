df2ac2697c868b96f3d7566b79f11f947e2c7ebf
ABQ <obeitoraq20@gmail.com>
2024-02-28 14:13:31 +0100

    landing page added

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  username: string = "";
+  password: string = "";
+
+  // constructor(private router: RouterOutlet) {}
+
+  login(): void {
+   
+  }
+}
